fa705b180ab7754590ac9b74e12842ad61d8317b
Faith <faithm@plus94.co.za>
2026-04-22 15:17:53 +0200

    Update work epic image loactions

--- client/dashboard.js
+++ client/dashboard.js
@@ -298,7 +298,7 @@ function displayData(data) {
             <div class="report-content hide" id="report-${index}">
               <div class="report-heading">
                 <img src="https://ubuntex.plus94.tech/assets/Plus94_logo2-CJbCr5Cg.png" alt="Plus94 Logo" class="logo">
-                <img src="https:workepic.netlify.app/assets/WorkEpic-DMG8B8qx.png" alt="Workepic Logo" class="logo">
+                <img src="https://workepic.netlify.app/assets/WorkEpic-DMG8B8qx.png" alt="Workepic Logo" class="logo">
               </div>
               <h3 class="report-name">Workepic Index Report for ${data.userProfile.firstName} ${data.userProfile.lastName}</h3>
               <div class="report-metadata">
