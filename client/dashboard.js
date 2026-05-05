4f71922af7a7c166967ef62412af0443d830ade9
Faith <codingfaith158@gmail.com>
2026-05-05 13:37:55 +0200

    Update image locations

--- client/dashboard.js
+++ client/dashboard.js
@@ -298,7 +298,7 @@ function displayData(data) {
             <div class="report-content hide" id="report-${index}">
               <div class="report-heading">
                 <img src="/Plus94_logo2.png" alt="Plus94 Logo" class="logo" id="report-logo">
-                <img src="/WorkEpic.png" alt="Workepic Logo" class="logo" id="w-epic">
+                <img src="/WorkEpic2.png" alt="Workepic Logo" class="logo" id="w-epic">
               </div>
               <h3 class="report-name">Workepic Report for ${data.userProfile.firstName} ${data.userProfile.lastName}</h3>
               <div class="report-metadata">
@@ -415,7 +415,7 @@ function displayAdminData(adminData) {
       <div class="admin-report-content hide" id="admin-report-${index}">
         <div class="report-heading">
           <img src="/Plus94_logo2.png" alt="Plus94 Logo" class="logo" id="report-logo">
-          <img src="/WorkEpic.png" alt="Workepic Logo" class="logo" id="w-epic">
+          <img src="/WorkEpic2.png" alt="Workepic Logo" class="logo" id="w-epic">
         </div>
         <h3 class="report-name">Workepic Report for ${adminData.userProfile?.firstName} ${adminData.userProfile?.lastName}</h3>
         <div class="report-metadata">
